f681b2c9439cdf4089bea5f65ccc3340232b2305
Jean-Paul Calderone <exarkun@divmod.com>
2009-08-19 17:12:34 -0400

    Handle spammed tickets with no other changes at all

--- trac-graphs/delete-comments-by.py
+++ trac-graphs/delete-comments-by.py
@@ -15,11 +15,15 @@ def main(dbfile, commenter):
     tickets = list(curs)
     curs.execute('DELETE FROM ticket_change WHERE author = ?', (commenter,))
     for (id,) in tickets:
+        curs.execute('SELECT MAX(time) FROM ticket_change WHERE ticket = ?', (id,))
+        results = list(curs.fetchall())
+        if len(results) == 0:
+            curs.execute('SELECT time FROM ticket WHERE id = ?', (id,))
+            changetime = list(curs.fetchall())[0][0]
+        else:
+            changetime = results[0][0]
         curs.execute(
-            'UPDATE ticket SET changetime = ('
-                'SELECT MAX(time) FROM ticket_change WHERE ticket = ?) '
-            'WHERE id = ?',
-            (id, id))
+            'UPDATE ticket SET changetime = ? WHERE id = ?', (changetime, id))
     conn.commit()
     conn.close()
 
